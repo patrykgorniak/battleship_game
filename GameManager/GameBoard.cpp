ae38811e6a8e9fc227dd4d62817bb5153b7894d8
Sebastian Gozdz <ssabuss@gmail.com>
2011-12-11 20:23:34 +0000

    Fixed problem with crashing game

--- GameManager/GameBoard.cpp
+++ GameManager/GameBoard.cpp
@@ -257,14 +257,14 @@ void GameBoard::generateBoard()
                     {
                         for(int i=0;i<m_size;i++)
                         {
-                            if(ship.getShipId() == fieldAt(i,ship.getPosition().second)) m_positions.removeOne(i*m_size + ship.getPosition().second);
+                            if(ship.getShipId() == fieldAt(i,ship.getPosition().second)) m_positionsGenerator.removeOne(i*m_size + ship.getPosition().second);
                         }
                     }
                     else
                     {
                         for(int i=0;i<m_size;i++)
                         {
-                            if(ship.getShipId() == fieldAt(ship.getPosition().first,i)) m_positions.removeOne(ship.getPosition().first*m_size + i);
+                            if(ship.getShipId() == fieldAt(ship.getPosition().first,i)) m_positionsGenerator.removeOne(ship.getPosition().first*m_size + i);
                         }
                     }
                 }
